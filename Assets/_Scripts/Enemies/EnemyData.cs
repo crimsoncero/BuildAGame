79099a35bf585c0d40cd8742ce96f8040258ad5a
Amit Breiman <crimson.breiman@gmail.com>
2025-01-04 11:47:31 +0200

    1

--- Assets/_Scripts/Enemies/EnemyData.cs
+++ Assets/_Scripts/Enemies/EnemyData.cs
@@ -3,5 +3,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData")]
 public class EnemyData : ScriptableObject
 {
-    
+    [field: Header("General")]
+    public string Name { get; private set; }
+
+
+    [field: Header("Base Stats")]
+    [field: SerializeField]
+    public int BaseMaxHealth { get; private set; }
+    [field: SerializeField]
+    public float BaseMaxSpeed { get; private set; } = 4f;
+    [field: SerializeField]
+    public int BasePower { get; private set; }
 }
