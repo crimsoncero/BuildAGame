b299d43c469d26a8f2820c23fee859ce985be379
Amit Breiman <93070078+crimsoncero@users.noreply.github.com>
2025-04-27 17:17:33 +0300

    1

---
+++ Assets/_Scripts/Player/Abiltiies/Editor/AbilityDataEditor.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+
+[CustomEditor(typeof(BaseAbilityData))]
+public class AbilityDataEditor : Editor
+{
+    
+    
+    
+}
+
+
+[CustomEditor(typeof(OrbitalAbilityData))]
+public class OrbitalAbilityDataEditor : AbilityDataEditor
+{
+    
+}
