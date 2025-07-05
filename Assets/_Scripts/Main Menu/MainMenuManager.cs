6fbf37ee987b3405967dd8112ce1068c216eb01e
Amit Breiman <crimson.breiman@gmail.com>
2025-07-05 13:54:15 +0300

    1

--- Assets/_Scripts/Main Menu/MainMenuManager.cs
+++ Assets/_Scripts/Main Menu/MainMenuManager.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 public class MainMenuManager : Singleton<MainMenuManager>
 {
     [field: SerializeField] public DataCollections DataCollections { get; private set; }
+    [field: SerializeField] public LevelInitData LevelInitData { get; private set; }
+    [SerializeField] private LevelSelection _levelSelection;
     
-    
+    public void OpenLevelSelection()
+    {
+        _levelSelection.Show();
+    }
+
+    public void OpenTeamSelection()
+    {
+        
+    }
 }
